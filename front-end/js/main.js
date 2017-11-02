63abdacd4dc71e6d9ef69459f327991c89a4a58e
jslegend <jslegend119@gmail.com>
2017-11-02 14:40:55 +0800

    Add files via upload

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -82,4 +82,131 @@ $(document).ready(function() {
   if ( $('[data-toggle="datepicker"]').length ) {
     $('[data-toggle="datepicker"]').datepicker();
   }
+
+  //Log in
+  $("#frm_signin").submit(function(event){
+    showSpin(true);
+    $.ajax({
+      data: {
+        submitStr:'loginSubmit',
+        email:$('#login_email').val(),
+        password:$('#login_password').val()
+      },
+      method: "POST",
+      url: $(this).attr('action'),
+      success: function(response){
+        var result = JSON.parse(response);
+        if(result.type == "Error")            //Signup Error
+          showLoginErr(result, true);
+        else{                                   //Signup Success
+          showLoginErr(result, false);
+          $.magnificPopup.close();
+          window.location.href = 'profile.php';
+        }
+        showSpin(false);
+      }  
+    });
+
+    event.preventDefault();
+  });
+
+  //Sing up
+  $("#frm_signup").submit(function(event){
+    showSpin(true);
+    $.ajax({
+      data: {
+        submitStr:'signupSubmit',
+        email:$('#signup_email').val(),
+        password:$('#signup_pwd').val(),
+        cnf_password:$('#signup_cnf_pwd').val()
+      },
+      method: "POST",
+      url: "src/login/userAccount.php",
+      success: function(response){
+        var result = JSON.parse(response);
+        if(result.type == "Error"){             //Signup Error
+          showSpin(false);
+          showSignupErr(result, true);
+        }
+        else{                                   //Signup Success
+          showSignupErr(result, false);
+          $.magnificPopup.close();
+          $.magnificPopup.open({items: {src: '#confirm-modal'},type: 'inline',closeOnBgClick:false});
+          sendEmail();
+        }
+      }  
+    });
+    event.preventDefault();
+  });
+
+  //Signup Err
+  function showSignupErr(result, show){
+    $('#span_err').text(result.type);
+    $('#span_msg').text(result.msg);
+    if(show)
+      $('.signup_err').show();
+    else
+      $('.signup_err').hide();
+  }
+  //Login Err
+  function showLoginErr(result, show){
+    $('#login_err').text(result.type);
+    $('#login_err_msg').text(result.msg);
+    if(show)
+      $('.login_err').show();
+    else
+      $('.login_err').hide();
+  }
+
+  //Email Send Function
+  function sendEmail(){
+    showSpin(true);
+    $.ajax({
+      data:{
+        sendEmail:true
+      },
+      method: "POST",
+      url: $('#email_send_link').attr('href'),
+      success: function(response){
+        showSpin(false);
+      }
+    });
+  }
+  
+  //Spin function
+  function showSpin(visible){
+    if(visible)
+      $('#spin').show();
+    else
+      $('#spin').hide();
+  }
+
+  $('#email_send_link').on("click", function(event){
+    sendEmail();
+    event.preventDefault();
+  });
+
+  $("#btn_logout").click( function(event){
+    
+    showSpin(true);
+    $.ajax({
+      data: {
+        submitStr:'logoutSubmit'
+      },
+      method: "POST",
+      url: "src/login/userAccount.php",
+      success: function(response){
+        var result = JSON.parse(response);
+        console.log(result);
+        window.location.href = 'index.php';
+        
+        showSpin(false);
+      }  
+    });
+
+    //event.preventDefault();
+  });
+
+
+
 });
