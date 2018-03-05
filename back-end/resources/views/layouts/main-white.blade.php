8867ce697459b78fa2d03ce8a71ee3c0cc956f63
Alexeyff <alexey.ffedorenko@gmail.com>
2018-03-05 11:23:53 +0200

    added meta desc / ga events

--- back-end/resources/views/layouts/main-white.blade.php
+++ back-end/resources/views/layouts/main-white.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="x-ua-compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Spend Bitcoin, Litecoin, Ethereum and Zantecoin in real life with just one card!">
 
     <title>ZANTEPAY</title>
 
