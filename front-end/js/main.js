e0dea960009ed82915ddecda2973ce4e95d26c74
Alexeyff <alexey.ffedorenko@gmail.com>
2018-04-26 21:59:30 +0300

    added mobile app page

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -456,4 +456,42 @@ $(document).ready(function() {
     $(this).toggleClass('is-active');
     $(this).siblings('ul').slideToggle();
   });
+
+
+  // ALPHA APP
+  //preloader
+  $(window).on('load', function() {
+    $('.app-preloader').addClass('is-hidden');
+    $('#blank-screen').addClass('is-zoomed');
+    setTimeout(function() {
+      $('#blank-screen').removeClass('is-active');
+      $('#start-screen').addClass('is-active')
+    }, 1600);
+  });
+
+  //flash animation for links
+  $(document).on('click', '.app-step img', function() {
+    var links = $(this).siblings('.link-area');
+    if ( !links.hasClass('is-active') ) {
+      links.addClass('is-active');
+      setTimeout(function() {
+        links.removeClass('is-active');
+      }, 1000);
+    }
+  });
+
+  //tabs
+  $(document).on('click', '.link-area', function(e) {
+    // e.preventDefault();
+    var tabId = $(this).attr('href');
+    $('.app-step').removeClass('is-active');
+    $(tabId).addClass('is-active');
+
+    //disable hash jumping
+    // var windowTop = $(window).scrollTop();
+    // setTimeout(function() {
+    //   window.scrollTo(0, windowTop);
+    // }, 1);
+    return false;
+  });
 });
