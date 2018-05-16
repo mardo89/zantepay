d90ea326adfaa613a9bbfa342a09143d79571372
Alexeyff <alexey.ffedorenko@gmail.com>
2018-05-16 23:46:43 +0300

    hp changes

--- back-end/resources/views/main/index.blade.php
+++ back-end/resources/views/main/index.blade.php
@@ -361,23 +361,27 @@
                     <h2 class="h2 headline text-left">Expert reviews</h2>
                 </div>
                 <div class="row p-t-60 mt-20 lp-rate text-center">
-                    <div class="col-md-4 mb-20">
+                    <div class="col-lg col-md-4 mb-20">
                         <a href="https://www.trackico.io/ico/zantepay/" target="_blank" rel="nofollow" title="ZANTEPAY on TrackICO">
                             <img border="0" src="https://www.trackico.io/widget/square/zantepay/400.png" width="150px" height="150px" alt="ZANTEPAY TrackICO rating">
                         </a>
                     </div>
-                    <div class="col-md-4 mb-20">
+                    <div class="col-lg col-md-4 mb-20">
                         <div>
                             <img src="images/icobench.png" width="200" alt="ICObench Logo">
                         </div>
-                        <a href="https://icobench.com/ico/zantepay" target="_blank" rel="nofollow" title="Zantepay on ICObench">
-                            <img border="0" src="https://icobench.com/rated/zantepay?shape=horizontal&size=m" width="200" alt="Zantepay ICO rating"/>
+                        <a href="https://icobench.com/ico/zantepay" target="_blank" rel="nofollow" title="Zantepay on ICObench"><img border="0" src="https://icobench.com/rated/zantepay?shape=horizontal&size=m" width="200" alt="Zantepay ICO rating"/></a>
+                    </div>
+                    <div class="col-lg col-md-4 mb-20">
+                        <a href="https://icomarks.com/ico/zantepay" target="_blank" rel="nofollow" title="Zantepay (PreICO)"><img border="0" src="https://icomarks.com/widget/z/zantepay/square.svg" width="150px" height="150px" alt="Zantepay (PreICO) ICO rating"/></a>
+                    </div>
+                    <div class="col-lg col-md-4 mb-20">
+                        <a href="https://www.coinschedule.com/ico/zantecoin" target="_blank" rel="nofollow" title="Zantepay Coinschedule">
+                            <img border="0" src="images/coinschedule.jpg" srcset="images/coinschedule@2x.jpg 2x" alt="Zantepay Coinschedule" style="border:1px solid #ccc;"/>
                         </a>
                     </div>
-                    <div class="col-md-4 mb-20">
-                        <a href="https://foundico.com/ico/zantepay.html" target="_blank" rel="nofollow" title="ZANTEPAY on Foundico.com">
-                            <img width="150" height="150" border="0" src="https://foundico.com/widget/?p=12067&f=s" alt="ZANTEPAY score on Foundico.com" />
-                        </a>
+                    <div class="col-lg col-md-4 mb-20">
+                        <a href="https://foundico.com/ico/zantepay.html" target="_blank" rel="nofollow" title="ZANTEPAY on Foundico.com"><img width="150" height="150" border="0" src="https://foundico.com/widget/?p=12067&f=s" alt="ZANTEPAY score on Foundico.com" /></a>
                     </div>
                 </div>
             </div>
