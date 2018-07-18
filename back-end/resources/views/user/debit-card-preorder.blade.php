1050c726b74d972ca7ab321317461e27e7242211
Alexeyff <alexey.ffedorenko@gmail.com>
2018-07-18 13:08:12 +0300

    hp/preorder card/wallet update

--- back-end/resources/views/user/debit-card-preorder.blade.php
+++ back-end/resources/views/user/debit-card-preorder.blade.php
@@ -15,7 +15,7 @@
             <div class="row">
                 <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
                     <div class="mb-20 text-center">
-                        <h2 class="h4">ZANTEPAY FREE debit card pre-order will be available shortly after ICO (after 15th July)</h2>
+                        <h2 class="h4">ZANTEPAY FREE debit card pre-order will be available shortly</h2>
                     </div>
                 </div>
             </div>
@@ -34,26 +34,7 @@
                     </div>
                 </div>
             </div>
-            <div class="text-center p-t-40">
-                <p class="h5">First 1000 card pre-orders will get rewarded 500 ZNX in bonus.</p>
-            </div>
-
-            <div class="row p-t-60">
-                <div class="col-lg-8 offset-lg-2">
-                    <h2 class="h4 headline-mb">Earn 500ZNX just by referring friends to pre-order ZANTEPAY card:</h2>
-                    <div class="row row-bottom" style="max-width: 650px;">
-                        <div class="col-md-7">
-                            <div class="field-group">
-                                <label class="field-label" for="field2">Referral link:</label>
-                                <input class="input-field" type="text" name="referral" value="https://test1.zantepay.com/invitation?ref=5afaa0a06d038" readonly="">
-                            </div>
-                        </div>
-                        <div class="col-xl-4 col-md-5">
-                            <button id="copy-link" type="button" class="mb-7 field-btn btn btn--shadowed-light btn--medium btn--full-w">Copy Link</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     </main>
 
